d9e3dea67b660b6a8c0da3ac44c29efb7396621c
David F <davidfonsecagaona@gmail.com>
2025-09-17 21:06:12 -0500

    fix: adding docker config. TS -> JS

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -23,7 +23,7 @@ passport.use(new LocalStrategy({
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID!,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    callbackURL: "/api/auth/google/callback",
+    callbackURL: process.env.GOOGLE_CALLBACK_URL!,
 }, async (_accessToken, _refreshToken, profile, done) => {
     try {
         const email = profile.emails?.[0].value;
